fb4faf7c026cf8d4a96d7ba72fb660e2eed3dcf7
Itz_jignesh <bsmarty863@gmail.com>
2025-11-04 11:39:08 +0530

    Restore and format SQL schema definitions

--- AOAEXP1.C
+++ AOAEXP1.C
@@ -38,11 +38,12 @@ VARCHAR(50), Brand VARCHAR(50), );
 CREATE TABLE DimCustomer (
  CustomerKey INT PRIMARY KEY, FirstName VARCHAR(50), LastName
 VARCHAR(50), Gender CHAR(1), Country VARCHAR(50), );
-Name:-Jignesh Bhatia. Batch:-A1. Roll No:-10
+
 CREATE TABLE DimStore (
  StoreKey INT PRIMARY KEY, StoreName VARCHAR(100), StoreType
 VARCHAR(50),Region VARCHAR(50));
 create table DimSupplier(
  SupplierKey int Primary key,SupplierType varchar(50));
 create table DimContact(
+
  ContactId int primary key,number int);
